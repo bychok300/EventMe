e9c64c0af21112c34f298d265ca7a1daf7c61932
Anton Bychek <bychok300@inbox.ru>
2017-10-03 15:12:27 +0300

    design changes

--- boards/admin.py
+++ boards/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Board, Post, Comments
+from .models import Board, Post, Comments, Topic
 
 admin.site.register(Board)
+admin.site.register(Topic)
 admin.site.register(Post)
 admin.site.register(Comments)
