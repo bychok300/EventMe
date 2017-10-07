2a4f7bff34a57d87aaa5b3745079925e594ca3b1
Anton Bychek <bychok300@inbox.ru>
2017-10-07 13:47:57 +0300

    bug at admin fixed

--- boards/models.py
+++ boards/models.py
@@ -11,6 +11,7 @@ class Board(models.Model):
     description = models.CharField(max_length=100)
 
     # этот метод - конструктор
+    # в данном случае будет отражать имя топика в админке
     def __str__(self):
         return self.name
 
@@ -21,6 +22,9 @@ class Topic(models.Model):
     board = models.ForeignKey(Board, related_name='topics')
     starter = models.ForeignKey(User, related_name='topics')
 
+    def __str__(self):
+        return self.subject
+
 
 class Post(models.Model):
     message = models.TextField(max_length=4000)
@@ -30,9 +34,15 @@ class Post(models.Model):
     created_by = models.ForeignKey(User, related_name='posts')
     updated_by = models.ForeignKey(User, null=True, related_name='+')
 
+    def __str__(self):
+        return self.topic.subject
+
 
 class Comments(models.Model):
     creator = models.ForeignKey(User, related_name='comment')
     body = models.TextField(max_length=4000)
     post = models.ForeignKey(Post, related_name='comment', null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body
